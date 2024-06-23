d36ca87213ddf073a6b71958654e39f12109e094
Md Ziad <mdujanpuc@gamil.com>
2024-06-23 16:52:43 +0600

    middleware added

--- index.js
+++ index.js
@@ -54,12 +54,23 @@ async function run() {
       res.send({ token });
     })
 
+      // middlewares 
+      const verifyToken = (req, res, next) => {
+        console.log('inside verify token', req.headers.authorization);
+        if (!req.headers.authorization) {
+          return res.status(401).send({ message: 'unauthorized access' });
+        }
+        const token = req.headers.authorization.split(' ')[1];
+        jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+          if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+          }
+          req.decoded = decoded;
+          next();
+        })
+      }
 
-    app.get("/package", async (req, res) => {
-      const cursor = packageCollection.find();
-      const result = await cursor.toArray();
-      res.send(result);
-    })
+
    
 // add packages  :--
 app.post('/package', async (req, res) => {
